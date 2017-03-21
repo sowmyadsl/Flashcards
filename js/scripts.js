3c3b9195d4a8cb0f4d0831ac7d0e817732d19969
James Higgins and Sowmya Dinavahi <sowmya@>
2017-03-21 15:05:31 -0700

    add ids to all p and h tags and add styling to css file

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,76 @@ $(document).ready(function() {
     $("p").hide();
     $("#def-java").show();
     });
-
     $("#h-op").click(function() {
       $("p").hide();
       $("#def-op").show();
       });
     $("#h-var").click(function() {
+      $("p").hide();
       $("#def-var").show();
-      $("p").removeClass();
       });
     $("#h-var-name").click(function() {
+      $("p").hide();
       $("#def-var-name").show();
-      $("p").removeClass();
       });
-
+    $("#h-fun").click(function() {
+      $("p").hide();
+      $("#def-fun").show();
+      });
+    $("#h-meth").click(function() {
+      $("p").hide();
+      $("#def-meth").show();
+      });
+    $("#h-arg").click(function() {
+      $("p").hide();
+      $("#def-arg").show();
+      });
+    $("#h-para").click(function() {
+      $("p").hide();
+      $("#def-para").show();
+      });
+    $("#h-ret").click(function() {
+      $("p").hide();
+      $("#def-ret").show();
+      });
+    $("#h-cha").click(function() {
+      $("p").hide();
+      $("#def-cha").show();
+      });
+    $("#h-str").click(function() {
+      $("p").hide();
+      $("#def-str").show();
+      });
+    $("#h-boo").click(function() {
+      $("p").hide();
+      $("#def-boo").show();
+      });
+    $("#h-un").click(function() {
+      $("p").hide();
+      $("#def-un").show();
+      });
+    $("#h-Nan").click(function() {
+      $("p").hide();
+      $("#def-Nan").show();
+      });
+    $("#h-esc").click(function() {
+      $("p").hide();
+      $("#def-esc").show();
+      });
+    $("#h-alert").click(function() {
+      $("p").hide();
+      $("#def-alert").show();
+      });
+    $("#h-com").click(function() {
+      $("p").hide();
+      $("#def-com").show();
+      });
+    $("#h-jq").click(function() {
+      $("p").hide();
+      $("#def-jq").show();
+      });
+    $("#h-attr").click(function() {
+      $("p").hide();
+      $("#def-attr").show();
+      });
 });
