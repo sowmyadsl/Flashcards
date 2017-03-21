b2d1ebeb4b643f392fc55337e7706f692e0fde3c
James Higgins and Sowmya Dinavahi <sowmya@>
2017-03-21 15:11:07 -0700

    work on formatting and correct layout

--- js/scripts.js
+++ js/scripts.js
@@ -2,77 +2,77 @@ $(document).ready(function() {
   $("#h-java").click(function() {
     $("p").hide();
     $("#def-java").show();
-    });
-    $("#h-op").click(function() {
-      $("p").hide();
-      $("#def-op").show();
-      });
-    $("#h-var").click(function() {
-      $("p").hide();
-      $("#def-var").show();
-      });
-    $("#h-var-name").click(function() {
-      $("p").hide();
-      $("#def-var-name").show();
-      });
-    $("#h-fun").click(function() {
-      $("p").hide();
-      $("#def-fun").show();
-      });
-    $("#h-meth").click(function() {
-      $("p").hide();
-      $("#def-meth").show();
-      });
-    $("#h-arg").click(function() {
-      $("p").hide();
-      $("#def-arg").show();
-      });
-    $("#h-para").click(function() {
-      $("p").hide();
-      $("#def-para").show();
-      });
-    $("#h-ret").click(function() {
-      $("p").hide();
-      $("#def-ret").show();
-      });
-    $("#h-cha").click(function() {
-      $("p").hide();
-      $("#def-cha").show();
-      });
-    $("#h-str").click(function() {
-      $("p").hide();
-      $("#def-str").show();
-      });
-    $("#h-boo").click(function() {
-      $("p").hide();
-      $("#def-boo").show();
-      });
-    $("#h-un").click(function() {
-      $("p").hide();
-      $("#def-un").show();
-      });
-    $("#h-Nan").click(function() {
-      $("p").hide();
-      $("#def-Nan").show();
-      });
-    $("#h-esc").click(function() {
-      $("p").hide();
-      $("#def-esc").show();
-      });
-    $("#h-alert").click(function() {
-      $("p").hide();
-      $("#def-alert").show();
-      });
-    $("#h-com").click(function() {
-      $("p").hide();
-      $("#def-com").show();
-      });
-    $("#h-jq").click(function() {
-      $("p").hide();
-      $("#def-jq").show();
-      });
-    $("#h-attr").click(function() {
-      $("p").hide();
-      $("#def-attr").show();
-      });
+  });
+  $("#h-op").click(function() {
+    $("p").hide();
+    $("#def-op").show();
+  });
+  $("#h-var").click(function() {
+    $("p").hide();
+    $("#def-var").show();
+  });
+  $("#h-var-name").click(function() {
+    $("p").hide();
+    $("#def-var-name").show();
+  });
+  $("#h-fun").click(function() {
+    $("p").hide();
+    $("#def-fun").show();
+  });
+  $("#h-meth").click(function() {
+    $("p").hide();
+    $("#def-meth").show();
+  });
+  $("#h-arg").click(function() {
+    $("p").hide();
+    $("#def-arg").show();
+  });
+  $("#h-para").click(function() {
+    $("p").hide();
+    $("#def-para").show();
+  });
+  $("#h-ret").click(function() {
+    $("p").hide();
+    $("#def-ret").show();
+  });
+  $("#h-cha").click(function() {
+    $("p").hide();
+    $("#def-cha").show();
+  });
+  $("#h-str").click(function() {
+    $("p").hide();
+    $("#def-str").show();
+  });
+  $("#h-boo").click(function() {
+    $("p").hide();
+    $("#def-boo").show();
+  });
+  $("#h-un").click(function() {
+    $("p").hide();
+    $("#def-un").show();
+  });
+  $("#h-Nan").click(function() {
+    $("p").hide();
+    $("#def-Nan").show();
+  });
+  $("#h-esc").click(function() {
+    $("p").hide();
+    $("#def-esc").show();
+  });
+  $("#h-alert").click(function() {
+    $("p").hide();
+    $("#def-alert").show();
+  });
+  $("#h-com").click(function() {
+    $("p").hide();
+    $("#def-com").show();
+  });
+  $("#h-jq").click(function() {
+    $("p").hide();
+    $("#def-jq").show();
+  });
+  $("#h-attr").click(function() {
+    $("p").hide();
+    $("#def-attr").show();
+  });
 });
