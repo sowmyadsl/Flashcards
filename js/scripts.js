634187a6f18bcac21a436c778a48fd58bbcab695
James Higgins and Sowmya Dinavahi <sowmya@>
2017-03-21 14:50:54 -0700

    Add .hide(); to the p tag to show only the definition on the page and hide the rest

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
   $("#h-java").click(function() {
+    $("p").hide();
     $("#def-java").show();
-    // $("p").removeClass();
-  });
+    });
+
+    $("#h-op").click(function() {
+      $("p").hide();
+      $("#def-op").show();
+      });
+    $("#h-var").click(function() {
+      $("#def-var").show();
+      $("p").removeClass();
+      });
+    $("#h-var-name").click(function() {
+      $("#def-var-name").show();
+      $("p").removeClass();
+      });
+
 });
